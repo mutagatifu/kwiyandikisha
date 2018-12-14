044de5bac6f57df9b1df1f2f4027c727c60cb924
HviewTech ltd <hviewtech.ltd@gmail.com>
2018-12-14 14:05:23 +0200

    with login

--- controller/login.php
+++ controller/login.php
@@ -9,6 +9,7 @@ include '../include/accounts.php';
 $login = new accounts();
 $usermail = $_POST['email'];
 $pword = $_POST['password'];
+$regId=$_POST['myid'];
 $login->setEmail($usermail);
 $login->setPassword($pword);
 
@@ -17,10 +18,11 @@ if(!empty($usermail) && !empty($pword)){
     $count = mysqli_num_rows($query);
     if ($count > 0){
         $user = $query->fetch_object();
-      $_SESSION['user'] = $user->fullName;
+//      $_SESSION['user'] = $user->fullName;
       $_SESSION['email'] = $user->email;
-      //header('Location:');
-        echo $_SESSION['user']." and ".$_SESSION['email'];
+      $_SESSION['requirements']=$regId;
+      header('Location:');
+      $_SESSION['user']." and ".$_SESSION['email'];
     }
     else{
         echo "username and password mismatch";
